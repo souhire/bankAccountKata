e29f9c9476d1eee3563ecc974d002e72c8dfde10
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-10 19:40:38 +0100

    Implémentation devs dépot et retrait

--- src/main/java/com/lcdlv/compte/Compte.java
+++ src/main/java/com/lcdlv/compte/Compte.java
@@ -1,17 +1,27 @@
 package com.lcdlv.compte;
 
+import java.util.ArrayList;
+
 public class Compte implements Account{
 
     private Double solde;
+    private ArrayList<Operation> historique;
+
+    public Compte() {
+        this.solde = Double.valueOf(0);
+        this.historique = new ArrayList<>();
+    }
 
     @Override
     public void depot(Double montant) {
-
+        this.solde += montant;
+        this.historique.add(new Depot(montant, this.solde));
     }
 
     @Override
     public void retrait(Double montant) {
-
+        this.solde -= montant;
+        this.historique.add(new Retrait(montant, this.solde));
     }
 
     @Override
