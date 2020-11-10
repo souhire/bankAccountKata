c83a05101002097a547217f934e3d4f24c54f423
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-10 14:34:24 +0100

    Initial commit

---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BankAccountTest {
+
+    @Test
+    public void testDepotSurCompte() {
+        fail("not implemented");
+    }
+
+    @Test
+    public void testRetraitSurCompte() {
+        fail("not implemented");
+    }
+
+    @Test
+    public void testHistoriqueDuCompte() {
+        fail("not implemented");
+    }
+
+
+}
