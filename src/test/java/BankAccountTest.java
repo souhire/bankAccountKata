e5d2b6a18b246f38c283574b224a95423dcc1600
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-10 19:35:27 +0100

    Implémentation tests dépot et retrait

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,26 +1,37 @@
 import com.lcdlv.compte.Compte;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankAccountTest {
 
     Compte compte;
+    Double solde;
+    Double montant;
 
     @BeforeAll
     public void setUp() {
         this.compte = new Compte();
+        this.solde = compte.getSolde();
+        this.montant = Math.random()*1000;
     }
 
     @Test
     public void testDepotSurCompte() {
-        fail("not implemented");
+        this.compte.depot(this.montant);
+        assertEquals(this.solde + this.montant, this.compte.getSolde());
+        this.solde += this.montant;
     }
 
     @Test
     public void testRetraitSurCompte() {
-        fail("not implemented");
+        this.compte.retrait(this.montant);
+        assertEquals(this.solde - this.montant, this.compte.getSolde());
+        this.solde -= this.montant;
     }
 
     @Test
